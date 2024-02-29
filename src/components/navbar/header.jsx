a59d431d55312366b4d4a8abfbac4004565f6585
Advaita <148793147+01adv@users.noreply.github.com>
2024-02-29 21:43:56 +0530

    hamburger aligned

--- src/components/navbar/header.jsx
+++ src/components/navbar/header.jsx
@@ -40,64 +40,33 @@ function Header() {
     >
       <nav class="bg-white border-gray-200 px-4 lg:px-6 py-3 dark:bg-black">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="/" class="flex items-center">
-            <img src={logo} class="mr-3 h-12 sm:h-20" alt="jgi logo" />
-            {/* <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Jai Gurudev Industries</span> */}
-          </a>
-          <div class="flex items-center lg:order-2">
-            {/* <a href="#" class="text-gray-700 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-base md:text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Contact</a> */}
-            <a
-              href="#"
-              class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-base md:text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-500 dark:hover:bg-primary-600 focus:outline-none dark:focus:ring-primary-700"
-            >
-              Become a Dealer
+          <div className="flex ">
+            <a href="/" class="flex items-center">
+              <img
+                src={logo}
+                class="mr-3 h-12 sm:h-16 lg:h-20"
+                alt="jgi logo"
+              />
             </a>
-
-            {/* //create a seperate file for phone hamburger */}
-            {/* <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
-              {<Hamburger/>}
-              <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className=" lg:hidden absolute top-4 right-3 sm:top-7">
+              <Hamburger />
+            </div>
+          </div>
+          <div class=" hidden lg:flex items-center lg:order-2">
+            <button>
+              <a
+                href="#"
+                class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-base md:text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-500 dark:hover:bg-primary-600 focus:outline-none dark:focus:ring-primary-700"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                class="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button> */}
-            <Hamburger />
+                Become a Dealer
+              </a>
+            </button>
           </div>
           <div
             class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 text-base md:text-lg">
-              {/* <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-600 lg:bg-transparent lg:text-primary-600 lg:p-0 dark:text-white" aria-current="page">Home</a>
-                    </li> */}
               <li>
                 <a
                   href="#"
